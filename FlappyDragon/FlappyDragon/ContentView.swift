d31074d98d8d2eb67380b954ff174b6a868e1782
Bill Hung <2024@billhung.net>
2024-01-28 00:37:32 +0900

    initially rotate 45 degrees, put the scale into variable dragonScale:Float

--- FlappyDragon/FlappyDragon/ContentView.swift
+++ FlappyDragon/FlappyDragon/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
     @State private var enlarge = false
     @State private var showImmersiveSpace = false
     @State private var immersiveSpaceIsShown = false
+    private let dragonScale:Float = 0.001
+    @State private var rotationAngle = 45
 
     @Environment(\.openImmersiveSpace) var openImmersiveSpace
     @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
@@ -26,7 +28,11 @@ struct ContentView: View {
             }
             // Add the dragon upon app load
             if let dragon = try? await Entity(named: "Low_Poly_Dragon"){
-                dragon.scale = [0.001,0.001,0.001]
+                dragon.scale = [dragonScale,dragonScale,dragonScale]
+//                dragon.scale = [repeating:dragonScale]
+//                dragon.rotate = [rotationAngle,rotationAngle,rotationAngle] //ERROR
+                // rotate the dragon by ___ degree
+                //dragon.transform.rotate(self: CGContext, by: 45.0)//rotateAngle(Angle:45) //error
                 content.add(dragon)
                 debugPrint("Low_Poly_Dragon added")
             }
@@ -35,8 +41,16 @@ struct ContentView: View {
             if let scene = content.entities.first {
                 let uniformScale: Float = enlarge ? 1.4 : 1.0
                 scene.transform.scale = [uniformScale, uniformScale, uniformScale]
+                debugPrint("scene enlarged")
             }
+            // if the dragon is clicked
+//            if let dragon = content.entities.first {
+//                let uniformScale: Float = enlarge ? 2.0 : 1.0
+//                dragon.transform.scale = [uniformScale, uniformScale, uniformScale]
+//                debugPrint("dragon enlarged")
+//            }
         }
+        .rotation3DEffect(.degrees(45.0), axis:(x:0,y:1,z:0))
         .onChange(of: showImmersiveSpace) { _, newValue in
             Task {
                 if newValue {
