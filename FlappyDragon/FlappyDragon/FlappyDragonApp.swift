324479554723bdfb04d135d4e9b254cadfb7d03e
熊ビル Bill Hung <2024@billhung.net>
2024-09-22 16:46:02 +0800

    add ViewModel.swift for the data model

--- FlappyDragon/FlappyDragon/FlappyDragonApp.swift
+++ FlappyDragon/FlappyDragon/FlappyDragonApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct FlappyDragonApp: App {
+    // The app's data model.
+    @State private var viewModel = ViewModel()
+    // Default code loading the app's view
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ FlappyDragon/FlappyDragon/ViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModel.swift
+//  FlappyDragon
+//
+//  Created by HC on 2024/09/22.
+//  Abstract:
+//  The app's data model.
+//
+
+import Foundation
+import RealityKit
+import RealityKitContent
+
+@Observable
+final class ViewModel {
+    
+}
